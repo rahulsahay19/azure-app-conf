a593701db1a5ddbe85c4119a737db9527315c5ea
Rahul Sahay <rahulsahay19@hotmail.com>
2020-03-29 19:24:57 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+namespace azure_app_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("ConnectionString"));
+
+            var config = builder.Build();
+            Console.WriteLine(config["TestApp:Settings:Message"] ?? "Hello world!");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Azure App Configuration 
+
+- This is console project to demo Azure App configuration. You can take this project and just use your connection string to test against Azure configuration. 
+
+- You can use $Env:ConnectionString = "connection-string" to set your connection string. 
+
+- Connection string you will get from access keys in App configuration app.
